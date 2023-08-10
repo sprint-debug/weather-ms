20a8eb3645a31d83dd73f3c796c1beccbdd99045
minsangKwak <kmsdevwork@gmail.com>
2023-08-10 22:16:39 +0900

    [fix] latitude, longtitude 값 가능하게 변경

--- src/Components/WeatherData.tsx
+++ src/Components/WeatherData.tsx
@@ -8,10 +8,11 @@ interface WeatherData {
 
 const WeatherDashboard: React.FC = () => {
   const [data, setData] = useState<WeatherData | null>(null);
+  const [latitude, setLatitude] = useState<string>('37.579617'); // 기본값으로 경복궁 위치 설정
+  const [longitude, setLongitude] = useState<string>('126.977041');
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords;
+    const fetchData = async () => {
       try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=73c0371a75f1ece71a255115ac4f3443&units=metric`);
         const json = await response.json();
@@ -23,16 +24,17 @@ const WeatherDashboard: React.FC = () => {
       } catch (err) {
         console.error(err);
       }
-    });
-  }, []);
+    };
+
+    fetchData();
+  }, [latitude, longitude]);
 
   return (
     <div>
       <div>
-          <h2>동네날씨</h2>
-          <p>Temperature: 10°C</p>
-          <p>Humidity: 100%</p>
-        </div>
+        Latitude: <input value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+        Longitude: <input value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+      </div>
       {data ? (
         <div>
           <h2>{data.name}</h2>
