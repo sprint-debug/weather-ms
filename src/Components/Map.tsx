d624f985ec68fa4c43d69f634b9d1b51388e9878
minsangKwak <kmsdevwork@gmail.com>
2023-08-13 03:22:14 +0900

    [feat] Map Component로 분리

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,51 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import './Map.css';
 
 declare global {
-    interface Window {
-        kakao: any;
-    }
+  interface Window {
+    kakao: any;
+  }
 }
 
 interface MapProps {
-    latitude: string;
-    longitude: string;
+  latitude: string;
+  longitude: string;
 }
 
-const mapStyle = {
-    width: '500px',
-    height: '500px',
-    border: '1px solid red'
-};
-
 const Map: React.FC<MapProps> = ({ latitude, longitude }) => {
-    const [map, setMap] = useState<any>(null);
+  const [map, setMap] = useState<any>(null);
 
-    useEffect(() => {
-        const { kakao } = window;
-        if (kakao && kakao.maps) {
-            const container = document.getElementById("map");
-            const options = {
-                center: new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude)),
-                level: 3
-            };
-            const createdMap = new kakao.maps.Map(container, options);
-            setMap(createdMap);
-        }
-    }, []);
+  useEffect(() => {
+    const { kakao } = window;
+    if (kakao && kakao.maps) {
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(
+          parseFloat(latitude),
+          parseFloat(longitude),
+        ),
+        level: 3,
+      };
+      const createdMap = new kakao.maps.Map(container, options);
+      setMap(createdMap);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (map) {
-            const { kakao } = window;
-            map.setCenter(new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude)));
-        }
-    }, [latitude, longitude, map]);
+  useEffect(() => {
+    if (map) {
+      const { kakao } = window;
+      map.setCenter(
+        new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude)),
+      );
+    }
+  }, [latitude, longitude, map]);
 
-    return (
-        <div>
-            <div id="map" style={mapStyle} />
-        </div>
-    );
+  return <div id="map" />;
 };
-
 
 export default Map;
